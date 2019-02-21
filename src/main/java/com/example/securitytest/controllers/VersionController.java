7f5fe4708d851cbe6db79cc926f444d5c0704276
Thomas Harper <thomas.l.harper.ctr@navy.mil>
2019-02-21 09:00:57 -0800

    Add library with webclient

--- src/main/java/com/example/securitytest/controllers/VersionController.java
+++ src/main/java/com/example/securitytest/controllers/VersionController.java
@@ -1,22 +1,14 @@
 package com.example.securitytest.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersionController {
 
-    @Autowired
-    JwtDecoder jwtDecoder;
-
     public VersionController() {
-        if (jwtDecoder == null) {
-            throw new RuntimeException("Unable to Autowire jwtDecoder");
-        }
     }
 
     @GetMapping(value="/v1/version")

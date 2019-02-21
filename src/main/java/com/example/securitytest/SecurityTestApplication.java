7582bc983ed3eeed73cb9f179c9b9b763dd28e2b
tlann <tlann@users.noreply.github.com>
2019-02-21 09:12:53 -0800

    Update SecurityTestApplication.java

--- src/main/java/com/example/securitytest/SecurityTestApplication.java
+++ src/main/java/com/example/securitytest/SecurityTestApplication.java
@@ -4,7 +4,6 @@ import com.example.utils.security.resource.annotations.EnableExampleResourceServ
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableExampleResourceServer
 @SpringBootApplication
 public class SecurityTestApplication {
 
